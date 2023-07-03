6b948ebc720770ca98592e82ddc59a9cdfb07689
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2023-07-03 15:25:47 +0900

    [level 0] Title: 숫자 찾기, Time: 5.64 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num, int k) {
+       String s = num + "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) - '0' == k) {
+                return i + 1;
+            }
+        }
+        return -1;
+    }
+}
