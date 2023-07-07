419d651900d6a5a3c0e06b4b24f5c822d2951664
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2023-07-07 20:56:09 +0900

    [level 0] Title: 대문자와 소문자, Time: 12.32 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -1,14 +1,13 @@
 class Solution {
     public String solution(String my_string) {
         String answer = "";
-        char[] chars = my_string.toCharArray();
-        for (Character c : chars) {
-            if (Character.isLowerCase(c)) {
-                answer += Character.toUpperCase(c);
+        char[] cArr = my_string.toCharArray();
+        for (char c : cArr){
+            if (Character.isUpperCase(c)){
+                answer+=Character.toLowerCase(c);
             } else {
-                answer += Character.toLowerCase(c);
+                answer+=Character.toUpperCase(c);
             }
-
         }
         return answer;
     }
