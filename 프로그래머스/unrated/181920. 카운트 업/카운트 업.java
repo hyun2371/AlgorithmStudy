f9d5a28c628bd5ecbec69af13acb927668d1cd2c
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2023-07-09 17:00:10 +0900

    [unrated] Title: 카운트 업, Time: 2.24 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,7 @@
+import java.util.stream.*;
+import java.util.*;
+class Solution {
+    public int[] solution(int start, int end) {
+        return IntStream.rangeClosed(start,end).toArray();
+    }
+}
