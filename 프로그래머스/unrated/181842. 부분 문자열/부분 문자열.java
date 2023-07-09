f168c2d6a7ebc6e889c124745b74a3a117cb797a
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2023-07-09 14:22:17 +0900

    [unrated] Title: 부분 문자열, Time: 0.02 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String str1, String str2) {
+        for (int i=0;i<=str2.length() -str1.length(); i++){
+            if (str1.equals(str2.substring(i,
+                                           i+str1.length())))             return 1;
+        }
+        
+        return 0;
+    }
+}
