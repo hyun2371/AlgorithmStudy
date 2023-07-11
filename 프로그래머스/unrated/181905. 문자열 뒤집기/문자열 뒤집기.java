59c3b131e79b17c6d0902f42bd55097d4d9ae82c
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2023-07-11 15:48:12 +0900

    [unrated] Title: 문자열 뒤집기, Time: 12.11 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,10 +1,8 @@
 class Solution {
     public String solution(String my_string, int s, int e) {
         
-        String r = my_string.substring(s,e+1);
-        StringBuffer r_sb = new StringBuffer(r);
-        String answer = my_string.substring(0,s)+r_sb.reverse()
+        StringBuffer r_sb = new StringBuffer(my_string.substring(s,e+1));
+        return my_string.substring(0,s)+r_sb.reverse()
             +my_string.substring(e+1,my_string.length());
-        return answer;
     }
 }
