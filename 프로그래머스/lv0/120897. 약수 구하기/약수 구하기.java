37d7e6e9b22bf5a60a1ff3f320ffce7971389e57
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2023-07-12 14:43:22 +0900

    [level 0] Title: 약수 구하기, Time: 2.26 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,12 @@
+import java.util.stream.*;
+import java.util.*;
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> lst = new ArrayList<>();
+        for (int i = 1;i<=n;i++){
+            if (n%i==0)
+                lst.add(i);
+        }
+        return lst.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
