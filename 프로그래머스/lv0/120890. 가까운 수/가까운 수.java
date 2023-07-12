276a86a4275d8536242d3ee7d3b2b3357f2b732c
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2023-07-12 20:08:07 +0900

    [level 0] Title: 가까운 수, Time: 0.39 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.java
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.java
@@ -1,15 +1,12 @@
 import java.util.*;
-import java.util.stream.*;
+
 class Solution {
     public int solution(int[] array, int n) {
         int min = 100;
-
+        Arrays.sort(array);
         for (int i = 0;i<array.length;i++){
             if (Math.abs(array[i]-n) < Math.abs(min-n)){
                 min=array[i];
-            }
-            else if (Math.abs(array[i]-n)==Math.abs(min-n)){
-                min=Math.min(min,array[i]);
             }
         }
         return min;
