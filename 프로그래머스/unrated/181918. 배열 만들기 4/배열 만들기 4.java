3bace86a322a9ba525f637abe062edbdeb4ac90a
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2023-07-18 19:22:46 +0900

    [unrated] Title: 배열 만들기 4, Time: 6.37 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
@@ -2,21 +2,25 @@ import java.util.*;
 import java.util.stream.*;
 class Solution {
     public int[] solution(int[] arr) {
+        Deque<Integer> stk = new LinkedList<>();
+        int i = 0;
         
-        List<Integer> stk = new ArrayList<>();
-        int i=0;
         while (i<arr.length){
-                if (stk.size()==0){
+            if (stk.isEmpty()){
                 stk.add(arr[i++]);
             } else {
-                if (stk.get(stk.size()-1)<arr[i]){
+                if (stk.peekLast()<arr[i]){
                     stk.add(arr[i++]);
-                } else {
-                    stk.remove(stk.size()-1);
+                } else{
+                    stk.pollLast();
                 }
             }
         }
-        
-        return stk.stream().mapToInt(x->x).toArray();
+
+        int[] answer = new int[stk.size()];
+        for (int ind =0;ind<answer.length;ind++){
+             answer[ind] = stk.pollFirst();
+        }
+        return answer;
     }
 }
