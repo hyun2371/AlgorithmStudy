ea0d3675269e700fdd8d52ff36e60cc5205f7020
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2023-07-21 19:34:57 +0900

    [level 0] Title: 등수 매기기, Time: 0.29 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
@@ -1,19 +1,15 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[][] score) {
-        int[] answer = new int[score.length];
-        int[] sc = new int[score.length];
-        
+        List<Integer> lst = new ArrayList<>();
         for (int i=0;i<score.length;i++){
-            sc[i] = score[i][0]+score[i][1];
+            lst.add(score[i][0]+score[i][1]);
         }
+        Collections.sort(lst,Collections.reverseOrder());
         
-        for (int i=0;i<sc.length;i++){
-            for (int j=0;j<sc.length;j++){
-                if (sc[i]<sc[j])
-                    answer[i]++;
-            }
-            answer[i]++;
+        int[] answer = new int[score.length];
+        for (int i=0;i<score.length;i++){
+            answer[i] = lst.indexOf(score[i][0]+score[i][1])+1;
         }
         return answer;
     }
