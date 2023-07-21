0c5a273b253b671327bb93ea4d40667291e34bd8
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2023-07-21 14:09:07 +0900

    [unrated] Title: 그림 확대, Time: 9.67 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181836. 그림 확대/그림 확대.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String[] solution(String[] picture, int k) {
+        String[] answer = new String[picture.length*k];
+        int ind =0;
+        
+        for (int i = 0; i<picture.length;i++){
+            String s = "";
+            
+            //answer원소
+            for (char c : picture[i].toCharArray()){
+                for (int j=0;j<k;j++)
+                    s+=c;
+            }
+            
+            //answer 원소 k번 삽입
+            for (int j = 0;j<k;j++)
+                answer[ind++] = s;
+            
+        }
+        return answer;
+    }
+}
