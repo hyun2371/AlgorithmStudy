3297b2b1a0f620af4fd2cf539d75874f101726be
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2023-08-04 22:00:16 +0900

    [level 1] Title: K번째수, Time: 5.00 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -5,11 +5,7 @@ class Solution {
 
         for (int r = 0; r<commands.length;r++){
             int i=commands[r][0]-1, j = commands[r][1]-1, k = commands[r][2]-1;
-            int[] tmp = new int[j-i+1]; int tmpInd = 0;
-            
-            for (int ind = i;ind<=j;ind++){
-                tmp[tmpInd++] = array[ind];
-            }
+            int[] tmp = Arrays.copyOfRange(array,i,j+1);
             Arrays.sort(tmp);
             answer[r] = tmp[k];
         }
