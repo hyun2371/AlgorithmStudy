a7d1c02409914e48c003750c9066d1d3a57ee629
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2023-08-05 14:49:39 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 8.52 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,14 +1,11 @@
 import java.util.*;
 class Solution {
     public String solution(int[] food) {
-        int n=0; StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         
         for (int i=1;i<food.length;i++){
-            if (food[i]%2!=0) n = (food[i]-1)/2;
-            else n = food[i]/2;
-            sb.append(String.valueOf(i).repeat(n));
+            sb.append(String.valueOf(i).repeat(food[i]/2));
         }
-        
          return sb.toString()+"0"+sb.reverse();
     }
 }
