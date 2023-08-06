e5fb6cb076c4030d524ada97f8bb39ae11f699c8
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2023-08-06 21:28:19 +0900

    [unrated] Title: 추억 점수, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -2,15 +2,13 @@ class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
         int[] answer = new int[photo.length]; 
         for (int i=0;i<photo.length;i++){
-            int score = 0;
             for (int j=0;j<photo[i].length;j++){
                 for (int k=0;k<name.length;k++){
                     if (photo[i][j].equals(name[k])){
-                        score+=yearning[k];
+                        answer[i]+=yearning[k];
                     }
                 }
             }
-            answer[i] = score;
         }
         return answer;
     }
