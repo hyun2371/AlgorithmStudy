962a8b1d73744d41202d35eabefe655f414d885b
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2024-06-22 21:37:22 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,18 +1,20 @@
 import java.io.*;
+//버블 정렬
 class Main {
     public int[] solution(String s) { 
         int[] arr = new int[s.length()];
         for (int i=0;i<s.length();i++){
             arr[i] = s.charAt(i)-'0';
         }
-
-        for (int i=1;i<arr.length;i++){
-            int tmp = arr[i], j;
-            for (j=i-1;j>=0;j--){
-                if (arr[j]<tmp) arr[j+1] = arr[j];
-                else break;
+        int n = arr.length;
+        for (int i=0;i<n-1;i++){
+            for (int j=0;j<n-i-1;j++){ //뒤에서 i번째만큼 이미 정렬됨(고정)
+                if (arr[j]<arr[j+1]){
+                    int tmp = arr[j];
+                    arr[j]= arr[j+1];
+                    arr[j+1] = tmp;
+                }
             }
-            arr[j+1] = tmp;
         }
         return arr;
     }
@@ -31,5 +33,6 @@ class Main {
         }
         br.close();
         bw.flush();
+        bw.close();
     }
 }
