ece5f50b47bb346310521bf7dd41142e7a3ba87a
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2024-07-10 22:32:30 +0900

    [level 1] Title: 문자열 나누기, Time: 0.79 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 1;
+
+        char c = s.charAt(0);
+        int cnt = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (cnt == 0) {
+                answer++;
+                c = s.charAt(i);
+            }
+            if (s.charAt(i)==c) cnt++;
+            else cnt--;
+        }
+
+        return answer;
+    }
+}
