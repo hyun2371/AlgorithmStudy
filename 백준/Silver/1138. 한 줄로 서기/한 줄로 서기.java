807ae66c8dafd796f2c6994a136f280f6d5b3f71
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2024-10-04 19:38:33 +0900

    [Silver II] Title: 한 줄로 서기, Time: 100 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -9,19 +9,21 @@ public class Main {
 		int[] arr = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
+		// 입력 받기
 		for (int i = 1; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		answer.add(N);
+
+		answer.add(N); // 최초 삽입
 		for (int i = N - 1; i > 0; i--) {
 			answer.add(arr[i], i);
 		}
 
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		for (Integer x : answer) {
-			bw.write(x + " ");
+		// 출력
+		StringBuilder sb = new StringBuilder();
+		for (Integer x : answer){
+			sb.append(x).append(" ");
 		}
-		bw.flush();
+		System.out.println(sb);
 	}
 }
