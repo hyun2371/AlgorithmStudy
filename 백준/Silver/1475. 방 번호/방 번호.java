eb311b93ac9b2b75972138c33044479852e16963
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-01-15 09:42:38 +0900

    [Silver V] Title: 방 번호, Time: 172 ms, Memory: 17784 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -6,11 +6,11 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		String s = String.valueOf(n);
-		int[] arr = new int[10]; // 빈도수 저장
+		int[] arr = new int[10]; 
 
 		for (int i = 0; i < s.length(); i++) {
 			char c = s.charAt(i);
-			if (c=='6'||c=='9'){ // 6,9는 치환 가능 -> 6,9가 나오면 두 빈도수 중 최솟값++
+			if (c=='6'||c=='9'){
 				if (arr[6]>arr[9]) arr[9]++;
 				else arr[6]++;
 			}
@@ -18,7 +18,7 @@ public class Main {
 				arr[c-'0']++;
 			}
 		}
-		// 정렬해서 가장 큰 값 반환
+
 		Arrays.sort(arr);
 		System.out.println(arr[9]);
 	}
