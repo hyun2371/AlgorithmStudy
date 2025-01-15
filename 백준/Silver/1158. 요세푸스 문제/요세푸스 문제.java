29c03c5f1fbb5c154c2e24dd86ca63259c6b95a1
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-01-15 10:53:41 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 288 ms, Memory: 16248 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -2,40 +2,28 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	private static List<Integer> lst = new ArrayList<>();
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
-		solution(N,K);
-		for (int i=0;i<lst.size();i++){
-			if (lst.size()==1)
-				System.out.printf("<%d>",lst.get(i));
-			else if (i==0){
-				System.out.print("<"+lst.get(i)+", ");
-			}
-			else if (i==lst.size()-1)
-				System.out.print(lst.get(i)+">");
-			else
-				System.out.print(lst.get(i)+", ");
-		}
-	}
 
-	private static void solution(int N, int K){
-		int count = 0;
-
-		Queue<Integer> q = new LinkedList<>();
+		Queue<Integer> q = new ArrayDeque<>();
 		for (int i = 1; i <= N; i++) {
 			q.offer(i);
 		}
 
-		while (!q.isEmpty()){
-			count++;
-			int x = q.poll();
-			if (count%K==0) lst.add(x);
-			else q.offer(x);
+		bw.write("<");
+		while(q.size()>1){
+			for (int i=0;i<K-1;i++){
+				q.offer(q.poll());
+			}
+			bw.write(q.poll()+", ");
 		}
-
+		bw.write(q.poll() + ">");
+		bw.flush();
+		bw.close();
 	}
+
 }
