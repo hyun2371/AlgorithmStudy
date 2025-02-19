7588a998babc2000921c2d73f9ef9502bf905c1f
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-02-19 16:02:36 +0900

    [level 2] Title: 삼각 달팽이, Time: 16.18 ms, Memory: 146 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,35 +1,26 @@
 class Solution {
+    final static int[] dx = {1,0,-1}; // 아래, 위, 왼쪽위
+    final static int[] dy = {0,1,-1};
+    
     public int[] solution(int n) {
         int[] answer = new int[n*(n+1)/2];
         int[][] arr = new int[n][n];
-        int x = 0, y=0, cnt = 1;
+        int x = 0, y=0, cnt = 1, d = 0;
         
         while (true){
-            while (true){
-                arr[x][y] = cnt++;
-                if (x+1==n||arr[x+1][y]!=0) break;
-                x++;
-             }
+            arr[x][y] = cnt++;
+            int nx = x+dx[d];
+            int ny = y+dy[d];
             
-            if (y+1==n||arr[x][y+1]!=0) break;
-            y++;
-            
-            while (true){
-                arr[x][y] = cnt++;
-                if (y+1==n||arr[x][y+1]!=0) break;
-                y++;
+            if (nx==n||ny==n||nx<0||ny<0||arr[nx][ny]!=0){ // 범위 넘어가거나 이미 방문
+                d = (d+1)%3; // 방향 바꿈
+                nx = x+dx[d]; // 방향 반영해 다시 계산
+                ny = y+dy[d];
+                if (nx==n||ny==n||nx<0||ny<0||arr[nx][ny]!=0) //범위 변경해도 같음 -> 다 채움
+                    break;
             }
-            if (arr[x-1][y-1]!=0) break;
-            x--; y--;
-            
-            while (true){
-                arr[x][y] = cnt++;
-                if (arr[x-1][y-1]!=0) break;
-                x--;y--;
-            }
-            
-            if (x+1==n||arr[x+1][y]!=0) break;
-            x++;
+            x=nx;
+            y=ny;
         }
         
         for (int i=0, ind=0;i<n;i++){
