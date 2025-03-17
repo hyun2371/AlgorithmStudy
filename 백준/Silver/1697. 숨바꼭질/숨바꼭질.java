cdd1339e2426927a6bbaf82dfd8a58b3063925d4
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-03-17 20:41:28 +0900

    [Silver I] Title: 숨바꼭질, Time: 144 ms, Memory: 23268 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,20 +1,22 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	private static int[] dis = new int[100_001];
+	static int[] dis = new int[100_001];
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
+		Arrays.fill(dis, -1);
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
+        
+        if (N==K) {
+			System.out.println(0); return;
+		}
+        
+		System.out.println(BFS(N, K));
 
-		Arrays.fill(dis,-1);
-		if (N==K)
-			System.out.println(0);
-		else 
-			System.out.println(BFS(N,K));
 	}
 
 	private static int BFS(int start, int target) {
@@ -22,16 +24,16 @@ public class Main {
 		q.offer(start);
 		dis[start] = 0;
 		while (!q.isEmpty()) {
-			int curX = q.poll();
-			int[] nxArr = {curX-1,curX+1,curX*2};
-			for (int nx : nxArr){
-				if (nx<0||nx>=100001) continue;
-				if (dis[nx]!=-1) continue;
+			int curX = q.poll(); // 현재 위치
+			int[] moves = {curX - 1, curX + 1, curX * 2};
+			for (int nx : moves) {
+				if (nx == target) return dis[curX] + 1;
+				if (nx < 0 || nx > 100_000 || dis[nx] >= 0) continue;
 				dis[nx] = dis[curX] + 1;
-				if (nx==target) return dis[nx];
 				q.offer(nx);
 			}
 		}
+		System.out.println(Arrays.toString(dis));
 		return -1;
 	}
 }
