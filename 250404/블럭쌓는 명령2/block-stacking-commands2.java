e05951b94bba5f6293ed13978c84d13fab425caf
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-04-04 23:11:02 +0900

    Title: 블럭쌓는 명령2, Time: 110ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250404/블럭쌓는 명령2/block-stacking-commands2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int[] block = new int[N];
+        for (int i = 0; i < K; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+
+            for (int j=A-1;j<=B-1;j++){
+                block[j]++;
+            }
+        }
+        Arrays.sort(block);
+        System.out.println(block[N-1]);
+    }
+}
