893563435aa0461836522bc7c11301cb1ca59412
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-04-07 17:40:17 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 147ms, Memory: 25MB, Status: Passed - Codetree

--- 250407/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.java
+++ 250407/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class Main {
     private static final int OFFSET = 1_000;
     private static final int LENGTH = 2_000;
-    private static boolean[][] area = new boolean[LENGTH+1][LENGTH+1];
+    private static int[][] area = new int[LENGTH+1][LENGTH+1];
     public static void main(String[] args) throws IOException{
         
         int[][] x = new int[3][2]; //직사각형 종류, 시작 끝
@@ -18,27 +18,23 @@ public class Main {
             x[i][1] = Integer.parseInt(st.nextToken())+OFFSET;
             y[i][1] = Integer.parseInt(st.nextToken())+OFFSET;
 
-            boolean flag;
-            if (i==2) flag = false;
-            else flag = true;
-
-            color(x[i][0],x[i][1],y[i][0],y[i][1],flag);
+            color(x[i][0],x[i][1],y[i][0],y[i][1],i+1);
         }
         
         int cnt = 0;
         for (int i=0;i<=LENGTH;i++){
             for (int j=0;j<=LENGTH;j++){
-                if (area[i][j]) cnt++;
+                if (area[i][j]==1||area[i][j]==2) cnt++;
             }
         }
         System.out.println(cnt);
     }
 
     // 범위만큼 직사각형 c로 칠하기
-    private static void color(int x1,int x2,int y1,int y2,boolean flag){
+    private static void color(int x1,int x2,int y1,int y2,int value){
         for (int r = x1; r<x2; r++){
             for (int c = y1;c<y2;c++){
-                area[r][c] = flag;
+                area[r][c] = value;
             }
         }
     }
