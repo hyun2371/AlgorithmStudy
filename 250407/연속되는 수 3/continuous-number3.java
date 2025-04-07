24e90cf83fa947e3e46c6f625d4f38d9dd478e7c
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-04-07 21:28:46 +0900

    Title: 연속되는 수 3, Time: 159ms, Memory: 11MB, Status: Passed - Codetree

--- 250407/연속되는 수 3/continuous-number3.java
+++ 250407/연속되는 수 3/continuous-number3.java
@@ -6,18 +6,19 @@ public class Main {
         int n = sc.nextInt();
         int[] arr = new int[n];
 
-        int cnt=1, answer = 1;
+
         for (int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
+        }
+        int cnt=1, answer = 1;
+        for (int i = 0; i < n; i++){
+           
             if (i>=1&&arr[i-1]*arr[i]>0){
                 cnt++;
             }
-            else {
-                answer = Math.max(cnt,answer);
-                cnt=1;
-            }
+            else cnt=1;
+            answer = Math.max(cnt,answer);
         }
-        answer = Math.max(cnt,answer);
         System.out.println(answer);
     }
 }
