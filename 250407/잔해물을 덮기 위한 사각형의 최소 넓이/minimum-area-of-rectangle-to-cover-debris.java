188d8ede53549b125094ce64a285548bb84ce5d2
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-04-07 18:45:24 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 106ms, Memory: 13MB, Status: Wrong Answer - Codetree

---
+++ 250407/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    private static final int OFFSET = 1000;
+    private static final int LEN = 2001;
+    public static void main(String[] args) throws IOException{
+        int[][] start = new int[2][2];
+        int[][] end = new int[2][2];
+        boolean[][] area = new boolean[LEN][LEN];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i=0;i<2;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            start[i][0]= Integer.parseInt(st.nextToken())+OFFSET;
+            start[i][1]=Integer.parseInt(st.nextToken())+OFFSET;
+            end[i][0]=Integer.parseInt(st.nextToken())+OFFSET;
+            end[i][1]=Integer.parseInt(st.nextToken())+OFFSET;
+            
+            for (int r=start[i][0];r<end[i][0];r++){
+                for (int c=start[i][1];c<end[i][1];c++){
+                    if (i==0) area[r][c]=true;
+                    else area[r][c] = false;
+                }
+            }
+        }
+
+        int minX=Integer.MAX_VALUE,maxX=Integer.MIN_VALUE,minY=Integer.MAX_VALUE,maxY=Integer.MIN_VALUE;
+        for (int r=0;r<LEN;r++){
+            for (int c=0;c<LEN;c++){
+                if (area[r][c]){
+                    minX = Math.min(r,minX);
+                    minY= Math.min(c,minY);
+                    maxX = Math.max(r,maxX);
+                    maxY = Math.max(c,maxY);
+                }
+            }
+        }
+        System.out.println((maxX+1-minX)*(maxY+1-minY));
+    }
+}
