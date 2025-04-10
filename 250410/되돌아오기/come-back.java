b446e229724be5645842c0f15b8768d2e84f93d5
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-04-10 21:31:29 +0900

    Title: 되돌아오기, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

--- 250410/되돌아오기/come-back.java
+++ 250410/되돌아오기/come-back.java
@@ -2,27 +2,19 @@ import java.util.Scanner;
 public class Main {
     private static int[] dx = {-1,0,0,1};
     private static int[] dy = {0,1,-1,0};
+    private static int totalSec=0,x=0,y=0,ans=-1;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int x = 0, y=0;
-        int totalSec=1;
         for(int i = 0; i < n; i++){
             char dir = sc.next().charAt(0);
             int dis = sc.nextInt();
             int dirNum = getDirNum(dir);
 
-            for (int j=1;j<=dis;j++,totalSec++){
-                x+=dx[dirNum];
-                y+=dy[dirNum];
-                if (x==0&&y==0){ //시작점 도착
-                    System.out.println(totalSec);
-                    System.exit(0);
-                }
-            }
+            boolean isDone = move(dis,dirNum);
+            if (isDone) break;
         }
-        System.out.println(-1);
-        
+        System.out.println(ans);
     }
 
     private static int getDirNum(char dir){
@@ -30,5 +22,19 @@ public class Main {
         if (dir=='E') return 1;
         if (dir=='W') return 2;
         else return 3;
+    }
+
+    private static boolean move(int dis, int dirNum){
+        while (dis-->0){
+            x+=dx[dirNum];
+            y+=dy[dirNum];
+
+            totalSec++;
+            if (x==0&&y==0) {
+                ans = totalSec;
+                return true;
+            }
+        }
+        return false;
     }
 }
