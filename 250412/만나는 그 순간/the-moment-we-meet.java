07704dcf2bc0c8dedc056fc71c19ec2435b6e87f
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-04-12 20:35:54 +0900

    Title: 만나는 그 순간, Time: 131ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250412/만나는 그 순간/the-moment-we-meet.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    static int[] a = new int[1001]; 
+    static int[] b = new int[1001];
+    static int t,aInd = 1, bInd = 1,pos;
+    static char dir;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // n번
+        int m = sc.nextInt();
+    
+        for (int i=0;i<n;i++){
+            dir = sc.next().charAt(0);
+            t = sc.nextInt();
+            moveA(dir,t);
+        }
+        pos=0;
+        for (int i=0;i<m;i++){
+            dir = sc.next().charAt(0);
+            t = sc.nextInt();
+            moveB(dir,t);
+        }
+
+        int time = -1;
+        for (int i=1;i<Math.max(aInd, bInd);i++){
+            if (a[i]==b[i]){
+                time = i; break;
+            }
+        }
+        System.out.println(time);
+    }
+
+    private static void moveA(char dir, int t){
+        while (t-->0){
+            if(dir=='R') pos++;
+            else pos--;
+            a[aInd++] = pos;
+        }
+    }
+
+    private static void moveB(char dir, int t){
+       while (t-->0){
+            if(dir=='R') pos++;
+            else pos--;
+            b[bInd++] = pos;
+        }
+    }
+}
