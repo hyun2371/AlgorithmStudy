b6fc45ddbbcee297d7176731a5eab83df1410580
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-04-22 21:35:56 +0900

    Title: 정수 명령 처리 6, Time: 90ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250422/정수 명령 처리 6/process-numeric-commands-6.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        while (N-->0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String cmd = st.nextToken();
+            if (cmd.equals("size")) {
+                System.out.println(pq.size());
+            }
+            if (cmd.equals("empty")){
+                System.out.println(pq.isEmpty()?1:0);
+            }
+            if (cmd.equals("pop")){
+                System.out.println(pq.poll());
+            }
+            if (cmd.equals("push")){
+                int x = Integer.parseInt(st.nextToken());
+                pq.add(x);
+            }
+            if (cmd.equals("top")){
+                System.out.println(pq.peek());
+            }
+        }
+        
+        
+    }
+}
