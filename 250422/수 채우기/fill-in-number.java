11f8efab3c59a6e94236f7a1e22d6148e2ff3d83
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-04-22 20:32:32 +0900

    Title: 수 채우기, Time: 133ms, Memory: 10MB, Status: Passed - Codetree

--- 250422/수 채우기/fill-in-number.java
+++ 250422/수 채우기/fill-in-number.java
@@ -4,17 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
- 
         int answer = Integer.MAX_VALUE;
      
-        for (int i=0;i<=n;i++){
-            int remain = n-5*i;
-            if (remain<0||remain%2!=0) continue;
-            
-            int tmp = i+remain/2;
+        for (int i=0;i<=n;i++){ // 5 구성 개수
+            int remain = n-5*i; //5로 구성 
+            if (remain<0) break;
+            if (remain%2!=0) continue;
+            int tmp = i+remain/2; // 나머지 2로 구성
             answer = Math.min(tmp,answer);
         }
-
         if (n%2!=0&&n<5) answer = -1; //2,5로 구성불가
         System.out.println(answer);
     }
