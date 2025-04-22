23f50f241c7ba20c7599b2f2e4068bd91736ec08
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-04-22 21:56:06 +0900

    Title: 최소 정수 출력, Time: 1586ms, Memory: 55MB, Status: Passed - Codetree

---
+++ 250422/최소 정수 출력/min-integer-output.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        while (n-->0){
+            int x = sc.nextInt();
+            if (x==0){
+                if (pq.isEmpty()) System.out.println(0);
+                else System.out.println(pq.poll());
+            }
+            else {
+                pq.offer(x);
+            }
+        }
+    }
+}
