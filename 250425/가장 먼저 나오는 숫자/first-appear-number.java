3079e9a5555ca3f482df71cc69cd8278a24bab4a
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-04-25 20:12:31 +0900

    Title: 가장 먼저 나오는 숫자, Time: 1781ms, Memory: 53MB, Status: Passed - Codetree

---
+++ 250425/가장 먼저 나오는 숫자/first-appear-number.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main {
+    private static int N;
+    private static int[] arr;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        int M = sc.nextInt();
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int[] queries = new int[M];
+        for (int i = 0; i < M; i++) {
+            int target = sc.nextInt();
+            System.out.println(lowerBound(target));
+        }
+        // Please write your code here.
+    }
+
+    private static int lowerBound(int target){
+        int lt = 0, rt = N-1;
+        int minIdx = N-1;
+        while (lt<=rt){
+            int mid = (lt+rt)/2;
+            if (arr[mid]>=target){
+                rt = mid-1;
+                minIdx = Math.min(mid, minIdx);
+            }
+            else lt = mid+1;
+        }
+        if (arr[minIdx]==target) return minIdx+1;
+        return -1;
+    }
+}
