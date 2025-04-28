14b7405286fa176da5dfe3f3e8997d948b748a64
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-04-28 12:57:15 +0900

    Title: 인접하지 않은 2개의 수, Time: 145ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250428/인접하지 않은 2개의 수/two-non-adjacent-numbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int maxSum = Integer.MIN_VALUE;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n;j++){
+                if (j==i-1||j==i+1||i==j) continue;
+                maxSum = Math.max(maxSum,arr[i]+arr[j]);
+            }
+        }
+        System.out.println(maxSum);
+    }
+}
