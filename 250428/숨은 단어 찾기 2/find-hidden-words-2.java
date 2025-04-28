fad762d6be7c2fc722886912337092a8f1657fe6
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-04-28 14:55:41 +0900

    Title: 숨은 단어 찾기 2, Time: 140ms, Memory: 10MB, Status: Passed - Codetree

--- 250428/숨은 단어 찾기 2/find-hidden-words-2.java
+++ 250428/숨은 단어 찾기 2/find-hidden-words-2.java
@@ -1,60 +1,53 @@
 import java.util.Scanner;
 public class Main {
-    static int n,m;
+    static int N,M;
+    static char[][] arr;
+    static int[] dx = {-1,1,0,0,-1,-1,1,1};
+    static int[] dy = {0,0,-1,1,-1,1,1,-1};
     public static void main(String[] args) {
+        initArr();
+
+        int answer = 0;
+        for (int i=0;i<N;i++){
+            for (int j=0;j<M;j++){
+                if (arr[i][j]!='L') continue;
+                for (int dirNum=0;dirNum<8;dirNum++){
+                    int curX = i;
+                    int curY = j;
+                    int eCnt = 0;
+                    while (true){
+                        int nx = curX + dx[dirNum];
+                        int ny = curY + dy[dirNum];
+                        
+                        if (!isRange(nx,ny)) break;
+                        if (arr[nx][ny]!='E') break;
+                        eCnt++; 
+                        if (eCnt==2) {
+                            answer++; break;
+                        }
+                        curX = nx; curY = ny;
+                    }
+                }
+            }
+        }
+        
+        System.out.println(answer);
+    }
+
+    private static boolean isRange(int x, int y){
+        return x>=0&&y>=0&&x<N&&y<M;
+    }
+
+    private static void initArr(){
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        char[][] arr = new char[n][m];
-        for (int i = 0; i < n; i++) {
+        N = sc.nextInt();
+        M = sc.nextInt();
+        arr = new char[N][M];
+        for (int i = 0; i < N; i++) {
             String s = sc.next();
-            for (int j=0;j<m;j++){
+            for (int j=0;j<M;j++){
                 arr[i][j] = s.charAt(j);
             }
         }
-
-        int cnt = 0;
-        for (int r=0;r<n;r++){
-            for (int c = 0; c<m;c++){
-                if (isPlusRange(r,n)){
-                    if (isLee(arr[r][c],arr[r+1][c],arr[r+2][c])) cnt++;
-                }
-                if (isPlusRange(c,m)){
-                    if (isLee(arr[r][c],arr[r][c+1],arr[r][c+2])) cnt++;
-                }
-                if (isMinusRange(r)){
-                    if (isLee(arr[r][c],arr[r-1][c],arr[r-2][c])) cnt++;
-                }
-                if (isMinusRange(c)){
-                    if (isLee(arr[r][c],arr[r][c-1],arr[r][c-2])) cnt++;
-                }
-
-                if (isPlusRange(r,n)&&isPlusRange(c,m)){
-                    if (isLee(arr[r][c],arr[r+1][c+1],arr[r+2][c+2])) cnt++;
-                }
-                if (isPlusRange(r,n)&&isMinusRange(c)){
-                    if (isLee(arr[r][c],arr[r+1][c-1],arr[r+2][c-2])) cnt++;
-                }
-                if (isMinusRange(r)&&isPlusRange(c,m)){
-                    if (isLee(arr[r][c],arr[r-1][c+1],arr[r-2][c+2])) cnt++;
-                }
-                if (isMinusRange(r)&&isMinusRange(c)){
-                    if (isLee(arr[r][c],arr[r-1][c-1],arr[r-2][c-2])) cnt++;
-                }
-            }
-        }
-        System.out.println(cnt);
-    }
-
-    private static boolean isPlusRange(int x, int size){
-        return x<=size-3;
-    }
-
-    private static boolean isMinusRange(int x){
-        return x>=2;
-    }
-
-    private static boolean isLee(char c1, char c2, char c3){
-        return c1=='L'&&c2=='E'&&c3=='E';
     }
 }
