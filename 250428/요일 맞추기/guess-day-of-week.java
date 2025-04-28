fe3f850d42596682a5990e0bd314e32ec35f61bf
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-04-28 19:23:58 +0900

    Title: 요일 맞추기, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250428/요일 맞추기/guess-day-of-week.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    static int[] dpm = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+    
+
+    public static void main(String[] args) {
+        String[] yoil = {"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        int term = getElapsedDay(m2,d2)-getElapsedDay(m1,d1);
+        String ans = term>=0?yoil[term%7]: yoil[7-Math.abs(term)%7];
+        System.out.println(ans);
+    }
+
+    private static int getElapsedDay(int month, int day){
+        int elapsedDay = 0;
+        for (int i=1;i<month;i++){
+            elapsedDay+=dpm[i];
+        }
+        elapsedDay+=day;
+        return elapsedDay;
+    }
+}
