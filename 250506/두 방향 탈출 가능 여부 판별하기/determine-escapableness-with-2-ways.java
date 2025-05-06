abb8dc47c077e071834b85f1c8dfbfdbcc1dc200
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-05-06 22:13:54 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 3029ms, Memory: 20MB, Status: Time Limit Exceed - Codetree

---
+++ 250506/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Main {
+    private static int N,M;
+    private static int[][] graph;
+    private static boolean[][] visited;
+    // 뱀 없음 1, 뱀 있음 0 방문함 0
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        graph = new int[N][M];
+        visited = new boolean[N][M];
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < M; j++)
+                graph[i][j] = sc.nextInt();
+        }     
+        DFS(0,0);
+        System.out.println(0);  
+    }
+
+    private static void DFS(int x, int y){
+        if (x==N-1&&y==M-1) {
+            System.out.println(1);
+            System.exit(0);
+        }
+        if (!inRange(x,y)||visited[x][y]||graph[x][y]==0) return;
+        visited[x][y] = true;
+        DFS(x+1,y);
+        DFS(x,y+1);
+        visited[x][y] = false;
+    }
+
+    private static boolean inRange(int x, int y){
+        return x>=0&&y>=0&&x<N&&y<M;
+    }
+}
