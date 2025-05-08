acc6757597ccf50e328546ee5b079d444e6e9ccc
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-05-08 22:18:53 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250508/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
@@ -0,0 +1,52 @@
+import java.util.*;
+public class Main {
+    private static boolean[][] visited;
+    private static int N,M;
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        visited = new boolean[N][M];
+
+        int[][] graph = new int[N][M];
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < M; j++)
+                graph[i][j] = sc.nextInt();
+        System.out.println(BFS(0,0));
+    }
+
+    private static int BFS(int x, int y){
+        Queue<Pos> q = new LinkedList<>();
+        q.offer(new Pos(x,y));
+        visited[x][y] = true;
+
+        while (!q.isEmpty()){
+            Pos cur = q.poll();
+            for (int d=0;d<4;d++){
+                int nx = cur.x+dx[d];
+                int ny = cur.y+dy[d];
+                if (nx==N-1&&ny==M-1) return 1;
+                if (!inRange(nx,ny)||visited[nx][ny]) continue;
+                visited[nx][ny] = true;
+                q.offer(new Pos(nx,ny));
+            }
+            
+        }
+        return 0;
+    }
+
+    private static boolean inRange(int x, int y){
+        return x>=0&&x<N&&y>=0&&y<N;
+    }
+}
+
+class Pos {
+    int x;
+    int y;
+    public Pos(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
