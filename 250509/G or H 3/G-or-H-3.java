2bff11d6aedc4ea7b14eae1fd0d0e48491517ec7
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-05-09 20:08:02 +0900

    Title: G or H 3, Time: 186ms, Memory: 10MB, Status: Passed - Codetree

--- 250509/G or H 3/G-or-H-3.java
+++ 250509/G or H 3/G-or-H-3.java
@@ -1,21 +1,19 @@
 import java.util.Scanner;
 public class Main {
+    static final int MAX_LEN = 10_000;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt(); // 인원 수
         int K = sc.nextInt(); // 사진 크기
-        int[] scores = new int[10_001];
-        int maxPos = 0;
+        int[] scores = new int[MAX_LEN+1];
         
         for (int i = 0; i < N; i++) {
             int pos = sc.nextInt();
             char c = sc.next().charAt(0);
             scores[pos] = (c=='G')?1:2;
-            maxPos = Math.max(maxPos,pos);
         }     
-
         int maxSum = 0;
-        for (int i=1;i<=maxPos-K+1;i++){
+        for (int i=1;i<=MAX_LEN-K;i++){
             int sum = 0;
             for (int j=i;j<=i+K;j++){
                 sum+=scores[j];
