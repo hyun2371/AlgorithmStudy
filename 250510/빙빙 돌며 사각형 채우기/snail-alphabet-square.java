fcd422c8e8562c31a97a512e1027db17af5dfa10
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-05-10 14:50:07 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 132ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250510/빙빙 돌며 사각형 채우기/snail-alphabet-square.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class Main {
+    static int[] dx = {0,1,0,-1};
+    static int[] dy = {1,0,-1,0};
+    static char[][] arr;
+    static int N,M;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        arr = new char[N][M];
+        boolean[][] visited = new boolean[N][M];
+        int dirNum = 3;
+        char ch = 'A';
+        
+        int x = 0,y = 0;
+        arr[x][y]=ch;
+        visited[x][y] = true; 
+        
+        for (int i=2;i<=N*M;i++){
+            int nx = x+dx[dirNum];
+            int ny = y+dy[dirNum];
+            ch = (char)(ch+1);      
+            if (!inRange(nx,ny)||visited[nx][ny]){
+                dirNum = (dirNum+1)%4;
+                nx = x+dx[dirNum];
+                ny = y+dy[dirNum];
+            }     
+            arr[nx][ny] = ch;
+            visited[nx][ny] = true;
+            x = nx; y = ny;         
+        }
+        
+        printAnswer(sc);
+        
+    }
+    private static boolean inRange(int x, int y){
+        return x>=0&&y>=0&&x<N&&y<M;
+    }
+
+    private static void printAnswer(Scanner sc){
+        for (int i=0;i<N;i++){
+            for (int j=0;j<M;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
