018f21e084b35538cd8d9d58486bddc2dadb901e
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-05-10 18:42:25 +0900

    Title: 선두를 지켜라 3, Time: 221ms, Memory: 28MB, Status: Wrong Answer - Codetree

--- 250510/선두를 지켜라 3/keep-the-lead-3.java
+++ 250510/선두를 지켜라 3/keep-the-lead-3.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 public class Main {
+    static int[] disA, disB;
+    static final int MAX_LEN = 1000_000;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int M = sc.nextInt();
+        disA = new int[MAX_LEN+1]; //시간별 거리
+        disB = new int[MAX_LEN+1]; 
         int indA = 1, indB = 1;
-        int[] disA = new int[1001]; //시간별 거리
-        int[] disB = new int[1001]; 
 
         for (int i = 0; i < N; i++) {
             int v = sc.nextInt(); //속도
@@ -26,8 +28,12 @@ public class Main {
             }
         }
 
-        int comb = 0;
+        System.out.print(getComb());
+    }
+
+    private static int getComb(){
         char status = ' ';
+        int comb = 0;
         for (int i=1;i<=14;i++){
             if (disA[i]==disB[i]&&status!='C'){
                 comb++;
@@ -42,6 +48,6 @@ public class Main {
                 status = 'B';
             }
         }
-        System.out.print(comb);
+        return comb;
     }
 }
