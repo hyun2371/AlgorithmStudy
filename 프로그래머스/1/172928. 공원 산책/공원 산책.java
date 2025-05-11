f9f9d1f72237900e4b3e6609f53c0f1469549e74
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-05-11 13:57:12 +0900

    [level 1] Title: 공원 산책, Time: 0.36 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -0,0 +1,65 @@
+import java.util.*;
+class Solution {
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+    static int[][] arr;
+    static int N,M, curX, curY; //arr 행, 열; 현재 x,y
+    
+    public int[] solution(String[] park, String[] routes) {
+        int[] answer = new int[2];
+        
+        //arr, 시작점 초기화
+        initArr(park,routes);
+        
+        for (int i=0;i<routes.length;i++){
+            int dirNum = getDirNum(routes[i].charAt(0));
+            int dis = (int)(routes[i].charAt(2)-'0');
+            move(dirNum,dis);
+        }
+        
+        answer[0] = curX;
+        answer[1] = curY;
+        return answer;
+    }
+    
+    private static void move(int dirNum, int dis){
+        int tmpX = curX; int tmpY = curY;
+        while (dis-->0){
+            tmpX = tmpX+dx[dirNum];
+            tmpY = tmpY+dy[dirNum];
+            if (!inRange(tmpX,tmpY)||arr[tmpX][tmpY]==0) return;
+        }
+        curX = tmpX;
+        curY = tmpY;
+    }
+    
+    private static boolean inRange(int x, int y){
+        return x>=0&&y>=0&&x<N&&y<M;
+    }
+    
+    private static int getDirNum(char c){
+        if (c=='N') return 0;
+        if (c=='S') return 1;
+        if (c=='W') return 2;
+        else return 3;
+    }
+    
+    private static void initArr(String[] park, String[] routes){
+        N = park.length;
+        M = park[0].length();
+        arr = new int[N][M];
+        for (int i=0;i<N;i++){
+            String s = park[i];
+            for (int j=0;j<M;j++){
+                if (s.charAt(j)=='S'){
+                    arr[i][j] = 1;
+                    curX = i; curY = j;
+                }
+                else if (s.charAt(j)=='O'){
+                    arr[i][j] = 1;
+                }
+                else arr[i][j] = 0;
+            }
+        }
+    }
+}
