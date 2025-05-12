8c1655b6325e5d9f72eb54a2dcaf6fa4f836ff34
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-05-12 22:09:14 +0900

    [level 1] Title: 덧칠하기, Time: 1.42 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,22 +1,12 @@
 import java.util.*;
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int cnt = 0, curInd = 1;
-        int[] arr = new int[n+1];
-        
-        for (int x : section){
-            arr[x] = 1; //칠해야하는 구역 표시
-        }
-
-        while (curInd<=n){
-            if (arr[curInd] == 1) {
+        int cnt = 0, cur = 0;
+        for (int x:section){
+            if (cur<x){
+                cur = x+m-1;
                 cnt++;
-                for (int i=curInd;i<curInd+m&&i<=n;i++){//칠하기
-                    arr[i] = 0;
-                }
-                curInd +=m; //칠한 구역 다음부터
             }
-            else curInd++;
         }
         return cnt;
     }
