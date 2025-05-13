30cc6976d06cc950c06bdb8823a1e89c16fb4e7c
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-05-13 15:07:25 +0900

    [level 1] Title: 모의고사, Time: 11.15 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 79.4 MB, 시간: 15.46 ms
+메모리: 84.1 MB, 시간: 11.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 11일 15:04:55
+2025년 05월 13일 15:07:23
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,35 +1,28 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] answers) {
-        int[] s1 = {1,2,3,4,5};  // 찍는 번호
-        int[] s2 = {2,1,2,3,2,4,2,5};
-        int[] s3 = {3,3,1,1,2,2,4,4,5,5};
-        int[] cnt = new int[3]; // 맞춘 횟수
+        int[] arr1 = {1,2,3,4,5};
+        int[] arr2 = {2,1,2,3,2,4,2,5};
+        int[] arr3 = {3,3,1,1,2,2,4,4,5,5};
         
-        // 맞춘 횟수 세기
-        for (int i=0;i < answers.length;i++){
-            if (s1[i%s1.length]==answers[i]) cnt[0]++;
-            if (s2[i%s2.length]==answers[i]) cnt[1]++;
-            if (s3[i%s3.length]==answers[i]) cnt[2]++;
+        int[] cnt = new int[3];
+        for (int i=0;i<answers.length;i++){
+            if (arr1[i%arr1.length]==answers[i]) cnt[0]++;
+            if (arr2[i%arr2.length]==answers[i]) cnt[1]++;
+            if (arr3[i%arr3.length]==answers[i]) cnt[2]++;
         }
         
-        // 최다득점자 구하기
-        String max_s_str = "";
-        int max = Arrays.stream(cnt).max().getAsInt(); //최고 점수
-     
+        List<Integer> lst = new ArrayList<>();
+        int max = Integer.MIN_VALUE;
         for (int i=0;i<cnt.length;i++){
-            if (cnt[i]==max){ // 동점자
-                max_s_str+=(i+1); // 사람(인덱스) 저장
+            if (cnt[i]==max) lst.add(i+1);
+            else if (cnt[i]>max){
+                max = cnt[i];
+                lst.clear();
+                lst.add(i+1);
             }
         }
         
-        // 최다득점자 문자열 -> int[]
-        int[] answer = new int[max_s_str.length()]; int answerInd=0;
-        for (char ind : max_s_str.toCharArray()){
-            answer[answerInd++] += ind-'0';
-        }
-
-        return answer;
+        return lst.stream().mapToInt(Integer::intValue).toArray();
     }
 }
