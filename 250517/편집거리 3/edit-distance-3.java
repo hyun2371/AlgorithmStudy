f7e213e9ac72ad0bf125138d8683bd7c1d39fd3a
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-05-17 21:34:44 +0900

    Title: 편집거리 3, Time: 134ms, Memory: 12MB, Status: Runtime Error - Codetree

---
+++ 250517/편집거리 3/edit-distance-3.java
@@ -0,0 +1,59 @@
+import java.util.*;
+public class Main {
+    static String s1, s2;
+    static int l1, l2;
+    static int[][] d;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        s1 = sc.next();
+        s2 = sc.next();
+        l1 = s1.length();
+        l2 = s2.length();
+        d = new int[l1][l2];
+
+        initFirstRow();
+        initFirstCol();
+       
+        for (int i=1;i<l1;i++){
+            for (int j=1;j<l2;j++){
+                if (s1.charAt(i)==s2.charAt(j)){
+                    d[i][j] = d[i-1][j-1]+1;
+                }
+                else {
+                    d[i][j] = Math.max(d[i-1][j],d[i][j-1]);
+                }
+            }
+        }
+
+        int lcsLen = d[l1-1][l2-1];
+        System.out.println((l1-lcsLen)+(l2-lcsLen));
+    }
+
+    private static void initFirstRow(){
+        // 1행 초기화 
+        // s1[0]==s2[i] => i부터 끝까지 d[0][i]=1로 초기화
+        char key = s1.charAt(0);
+        for (int i=0;i<l2;i++){
+            if (key==s2.charAt(i)){
+                for (int j=i;j<l2;j++){
+                     d[0][j] = 1;
+                }
+                break;  
+            }
+            d[0][i] = 0;
+        }
+    }
+    private static void initFirstCol(){
+         // 1열 초기화
+        char key = s2.charAt(0);
+        for (int i=0;i<l1;i++){
+            if (key==s1.charAt(i)){
+                for (int j=i;j<l2;j++){
+                     d[j][0] = 1;
+                }
+                break;  
+            }
+            d[i][0] = 0;
+        }
+    }
+}
