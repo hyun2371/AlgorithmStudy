030548c92bcbf37c6e20bff34dd22d819c0924bf
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-06-02 19:17:11 +0900

    [Silver I] Title: 회전 초밥, Time: 244 ms, Memory: 23400 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -1,6 +1,5 @@
-import java.util.*;
 import java.io.*;
-
+import java.util.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,35 +10,32 @@ public class Main {
 		int K = Integer.parseInt(st.nextToken()); //연속개수
 		int C = Integer.parseInt(st.nextToken()); //쿠폰번호
 
-		int[] arr = new int[2*N];
+		int[] arr = new int[N+K];
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
 
-		for (int i = 0; i < N; i++) {//원형
+		for (int i = 0; i < K; i++) {//원형
 			arr[N+i] = arr[i];
 		}
 
-		Set<Integer> set = new HashSet<>();
-		Map<Integer, Integer> sushi = new HashMap<>(); //초밥번호, 개수 -> 2개 이상일때는 중복 없애되, 0개이면 제거하기 위해
+		Map<Integer, Integer> map = new HashMap<>(); //초밥번호, 개수 -> 2개 이상일때는 중복 없애되, 0개이면 제거하기 위해
 
 		for (int i = 0; i < K; i++) {
-			set.add(arr[i]);
-			sushi.put(arr[i],sushi.getOrDefault(arr[i],0)+1);
+			map.put(arr[i],map.getOrDefault(arr[i],0)+1);
 		}
 
 		int answer = 0;
-		for (int rt = K,lt=0; rt <2*N; rt++,lt++) {
+		for (int rt = K,lt=0; rt <arr.length; rt++,lt++) {
 			//오른쪽 스시 추가
-			set.add(arr[rt]);
-			sushi.put(arr[rt],sushi.getOrDefault(arr[rt],0)+1);
-			
-			//왼쪽 스시 제거
-			sushi.put(arr[lt],sushi.getOrDefault(arr[lt],0)-1);
-			if (sushi.get(arr[lt])==0) set.remove(arr[lt]); //윈도우 안에 초밥 없으면 set에서도 제거
+			map.put(arr[rt],map.getOrDefault(arr[rt],0)+1);
 
-			set.add(C); // 쿠폰 스시 추가
-			answer = Math.max(set.size(), answer);
+			//왼쪽 스시 제거
+			map.put(arr[lt],map.getOrDefault(arr[lt],0)-1);
+			if (map.get(arr[lt])==0) map.remove(arr[lt]); //윈도우 안에 초밥 없으면 set에서도 제거
+
+			map.put(C,map.getOrDefault(C,0)+1);
+			answer = Math.max(map.size(), answer);
 		}
 		System.out.println(answer);
 	}
