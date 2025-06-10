5eab67a015edae173e70a1b1984de66ed6da8fd3
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-06-10 13:00:50 +0900

    [level 1] Title: 숫자 짝꿍, Time: 75.26 ms, Memory: 165 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -1,33 +1,28 @@
 import java.util.*;
 class Solution {
-    static int[] cnt1 = new int[10];
-    static int[] cnt2 = new int[10];
     public String solution(String X, String Y) {
-        initCnt1(X);
-        initCnt2(Y);
+        int[] cnt1 = new int[10];// 인덱스가 몇 번 나왔는지
+        int[] cnt2 = new int[10];
+        initCnt(cnt1, X);
+        initCnt(cnt2, Y);
         
         StringBuilder sb = new StringBuilder();
         for (int i=9;i>=0;i--){
-            int interCnt = Math.min(cnt1[i],cnt2[i]);
+            int interCnt = Math.min(cnt1[i],cnt2[i]); //교집합
             while (interCnt-- >0){
-                if (i==0&&sb.length()==0) return "0"; // 0이 최초로 나오는 경우
                 sb.append(i);
             }
         }
+          
         if (sb.length()==0) return "-1";
+        if (sb.charAt(0)=='0') return "0";
         
         return sb.toString();
     }
     
-    private static void initCnt1(String X){
-        for (char c : X.toCharArray()){
-            cnt1[c-'0']++;
+    private static void initCnt(int[] cnt, String s){
+        for (char c : s.toCharArray()){
+            cnt[c-'0']++;
         }
-    } 
-    
-    private static void initCnt2(String Y){
-        for (char c : Y.toCharArray()){
-            cnt2[c-'0']++;
-        }
-    } 
+    }  
 }
