05676460bd7fe6773edaa5a4d34793a7683794cb
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-06-16 13:31:29 +0900

    [level 2] Title: 큰 수 만들기, Time: 53.62 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -3,10 +3,10 @@ import java.util.*;
 class Solution {
     public String solution(String number, int k) {
 
-        Stack<Integer> stk = new Stack<>();
+        Stack<Character> stk = new Stack<>();
         
         for (int i=0;i<number.length();i++){
-            int cur = number.charAt(i)-'0';
+            char cur = number.charAt(i);
             
             while (!stk.isEmpty()&&stk.peek()<cur&&k>0){
                 stk.pop(); k--;
@@ -15,14 +15,11 @@ class Solution {
         }
         
         StringBuilder sb = new StringBuilder();
-        for (int x:stk){
-            sb.append(x);
+
+        for (char c : stk){
+            sb.append(c);
         }
         
-        if (k>0){
-            return sb.substring(0,sb.length()-k);
-        }
-        
-        return sb.toString();
+        return sb.substring(0,sb.length()-k);
     }
 }
