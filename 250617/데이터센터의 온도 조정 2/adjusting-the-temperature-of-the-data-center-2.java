f21580cc7be03b34d7d1e78137221266800c2147
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-06-17 10:25:45 +0900

    Title: 데이터센터의 온도 조정 2, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

--- 250617/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.java
+++ 250617/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.java
@@ -1,32 +1,38 @@
 import java.util.*;
 public class Main {
-    private static int C,G,H;
+    private static int N,C,G,H;
+    private static int[] ta,tb;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        N = sc.nextInt();
         C = sc.nextInt();
         G = sc.nextInt();
         H = sc.nextInt();
-        int[] ta = new int[n];
-        int[] tb = new int[n];
+        ta = new int[N];
+        tb = new int[N];
         int minLimit = Integer.MAX_VALUE;
         int maxLimit = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < N; i++) {
             ta[i] = sc.nextInt();
             tb[i] = sc.nextInt();
             minLimit = Math.min(minLimit,ta[i]);
             maxLimit = Math.max(maxLimit, tb[i]);
         }
 
-        int maxScore = 0;
-        for (int i=minLimit-1;i<=maxLimit+1;i++){
-            int score = 0;
-            for (int j=0;j<n;j++){
-                score+=getScore(i,ta[j],tb[j]);
-            }
-            maxScore = Math.max(maxScore, score);
+        int maxTotalScore = 0;
+        for (int num=minLimit-1;num<=maxLimit+1;num++){
+            int totalScore = getTotalScore(num);
+            maxTotalScore = Math.max(maxTotalScore, totalScore);
         }
-        System.out.println(maxScore);
+        System.out.println(maxTotalScore);
+    }
+
+    private static int getTotalScore(int num){
+        int totalScore = 0;
+        for (int ind=0;ind<N;ind++){
+            totalScore+=getScore(num,ta[ind],tb[ind]);
+        }
+        return totalScore;
     }
 
     private static int getScore(int num,int t1, int t2){
