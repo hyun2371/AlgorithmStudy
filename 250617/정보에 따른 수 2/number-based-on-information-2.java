40bc31528308ce58507e7803521a76067b735b3e
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-06-17 13:27:41 +0900

    Title: 정보에 따른 수 2, Time: 147ms, Memory: 10MB, Status: Passed - Codetree

--- 250617/정보에 따른 수 2/number-based-on-information-2.java
+++ 250617/정보에 따른 수 2/number-based-on-information-2.java
@@ -1,58 +1,39 @@
-import java.util.*;
+import java.util.Scanner;
 public class Main {
-    static int minDis1,minDis2;
-    static List<Pos> lst = new ArrayList<>();
-    static int[][] pos;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         int a = sc.nextInt();
         int b = sc.nextInt();
+        int[] sPos = new int[100];
+        int[] nPos = new int[100];
+        int sInd = 0, nInd = 0;
 
         for (int i = 0; i < T; i++) {
-            char alpha = sc.next().charAt(0);
-            int x = sc.nextInt();
-            lst.add(new Pos(alpha,x));
-        }
 
-        int cnt = 0;
-        for (int k=a;k<=b;k++){
-            getDis(k);
-            if (minDis1<=minDis2)cnt++;
-        }
-        System.out.println(cnt);
-        
-        
-    }
-
-    private static void getDis(int k){
-        minDis1 = Integer.MAX_VALUE;
-        minDis2 = Integer.MAX_VALUE;
-        for (int i=0;i<lst.size();i++){
-            Pos p = lst.get(i);
-            if (p.alpha=='S'){
-                int dis1 = Math.abs(p.x-k);
-                minDis1 = Math.min(minDis1, dis1);
-               
-            } else{
-                int dis2 = Math.abs(p.x-k);
-                minDis2 = Math.min(minDis2, dis2);
+            char c = sc.next().charAt(0);
+            int pos = sc.nextInt();
+            if (c=='S'){
+                sPos[sInd++] = pos;
+            } else {
+                nPos[nInd++] = pos;
             }
         }
         
-    }
-}
+        int cnt = 0;
+       for (int k=a;k<=b;k++){
+            int minDisS = Integer.MAX_VALUE;
+            int minDisN = Integer.MAX_VALUE;
 
-class Pos implements Comparable<Pos>{
-    char alpha;
-    int x;
+            for (int i=0;i<sInd;i++){
+                minDisS = Math.min(minDisS,Math.abs(sPos[i]-k));
+            }
 
-    public Pos(char alpha,int x){
-        this.alpha = alpha;
-        this.x = x;
-    }
-
-    public int compareTo(Pos e){
-        return this.x-e.x;
+            for (int i=0;i<nInd;i++){
+                minDisN = Math.min(minDisN,Math.abs(nPos[i]-k));
+            }
+            if (minDisS<=minDisN) cnt++;
+       }
+       System.out.println(cnt);
     }
 }
