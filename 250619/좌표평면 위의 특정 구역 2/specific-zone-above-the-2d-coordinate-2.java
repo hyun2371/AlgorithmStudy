af16a5f61ac4703e5630332aba90b8715cd492cf
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-06-19 13:31:24 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 133ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250619/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] x = new int[N];
+        int[] y = new int[N];
+        for (int i = 0; i < N; i++) {
+            x[i] = sc.nextInt();
+            y[i] = sc.nextInt();
+        }
+
+        
+
+        int answer = Integer.MAX_VALUE, tmp=0;
+        for (int i=0;i<N;i++){ //제외할 사각형
+            int maxX = Integer.MIN_VALUE;
+            int maxY = Integer.MIN_VALUE;
+            int minX = Integer.MAX_VALUE;
+            int minY = Integer.MAX_VALUE;
+
+            for (int j=0;j<N;j++){
+                if (i==j) continue;
+                minX = Math.min(minX,x[j]);
+                maxX = Math.max(maxX,x[j]);
+                minY = Math.min(minY,y[j]);
+                maxY = Math.max(maxY,y[j]);
+                tmp = (maxX-minX)*(maxY-minY);
+            }
+            
+            
+            answer = Math.min(answer,tmp);
+        }
+        System.out.println(answer);
+    }
+}
