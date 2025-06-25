a4865353aa58a82338b8422d4cc20f66bac25063
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-06-25 10:10:47 +0900

    Title: 행복한 수열의 개수, Time: 132ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250625/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+  
+public class Main {
+    static int N,M;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        N = sc.nextInt();
+        M = sc.nextInt();
+        int[][] arr = new int[N][N];
+
+        int answer = 0;
+        for (int i=0;i<N;i++){
+            for (int j=0;j<N;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int r=0;r<N;r++){
+            if (isSerial(arr[r])) answer++;
+        }
+        
+        for (int c=0;c<N;c++){
+            int[] line = new int[N];
+            for (int r =0;r<N;r++){
+                line[r] = arr[r][c];
+            }
+            if (isSerial(line)) answer++;
+        }
+        System.out.println(answer);
+    }
+
+    private static boolean isSerial(int[] line){
+        int cnt = 0, tmp =-1;
+        for (int i=0;i<N;i++){
+            if (line[i]!=tmp){
+                tmp = line[i];
+                if (cnt>=M) return true;
+            }
+            else cnt++;
+        }
+        return false;
+    }
+}
