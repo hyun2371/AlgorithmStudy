9015e7a9f98db524cd0d6163d53c80e5c2a84b47
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-07-03 15:09:45 +0900

    [level 2] Title: 모음 사전, Time: 12.34 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -5,18 +5,19 @@ class Solution {
     static StringBuilder sb  = new StringBuilder();
     static String[] arr = {"A","E","I","O","U"};
     public int solution(String word) {
-        DFS(0);
+        DFS(0,word);
         return lst.indexOf(word)+1;
     }
     
-    private static void DFS(int lv){
+    private static void DFS(int lv,String word){
         if (lv==5){
             return;
         }
-        for (int i=0;i<5;i++){
+        for (int i=0;i<arr.length;i++){
             sb.append(arr[i]);
             lst.add(sb.toString());
-            DFS(lv+1);
+            if (sb.toString().equals(word)) return;
+            DFS(lv+1,word);
             sb.setLength(sb.length() - 1);
         }
     }
