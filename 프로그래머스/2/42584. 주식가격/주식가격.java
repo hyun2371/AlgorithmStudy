e289345954e0c32eb7749b038e37602f6792e5a8
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-07-07 14:04:10 +0900

    [level 2] Title: 주식가격, Time: 17.31 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -5,10 +5,9 @@ class Solution {
         for (int i=0;i<answer.length;i++){
             int cnt = 0;
             for (int j=i+1;j<answer.length;j++){
-                cnt++;
+                answer[i]++;
                 if (prices[j]<prices[i]) break;
             }
-            answer[i] = cnt;
         }
         return answer;
     }
