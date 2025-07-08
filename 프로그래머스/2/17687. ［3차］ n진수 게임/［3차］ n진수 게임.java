e9142099c10c13d39cbd2def1e1866d245183011
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-07-08 12:09:59 +0900

    [level 2] Title: [3차] n진수 게임, Time: 433.00 ms, Memory: 378 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -2,33 +2,18 @@ import java.util.*;
 class Solution {
     public String solution(int n, int t, int m, int p) {
         
-        char[] arr = new char[m*t]; //0-based
-        initArr(arr,n,m,t);
+        //0부터 진법 변환해서 더하기
+        String total = ""; int num = 0;
+        while (total.length()<=m*t){
+            total+= Integer.toString(num++,n).toUpperCase();
+        }
         
         //순서에 맞는 숫자 구하기
         StringBuilder sb = new StringBuilder();
         for (int i=0;i<t;i++){
-            sb.append(arr[m*i+p-1]);
+            sb.append(total.charAt(m*i+p-1));
         }
 
         return sb.toString();
-    }
-    
-    // 진수 변환해서 한자리씩 배열에 넣어주기
-    private static void initArr(char[] arr, int n, int m, int t){
-        arr[0] = '0'; //0부터 시작
-        int ind = 1, num = 1;
-        boolean flag = false;
-        
-        while (true){
-            String s = Integer.toString(num++,n).toUpperCase();
-            for (int i=0;i<s.length();i++){
-                if (ind==m*t) { //배열 길이만큼 구하기
-                    flag = true; break;
-                }
-                arr[ind++] = s.charAt(i);
-            }
-            if (flag) break;
-        }
     }
 }
