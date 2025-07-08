252a5e59d5233d7bb6874e085685c01d869e4a46
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-07-08 10:15:42 +0900

    [level 2] Title: 롤케이크 자르기, Time: 337.15 ms, Memory: 176 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,23 +1,30 @@
 import java.util.*;
+/**
+ * 접근 방식
+ * - 수량 계속 관리해야 하므로 map 사용
+ * - 처음에 mapB에게 모든 토핑을 할당하고 mapB의 토핑을 뺏어서 mapA에게 주는 방식
+ * 헷갈렸던 점
+ * - map.get.keySet(x).size()로만 비교하면 수량이 0인 경우까지 카운트됨
+ * - 수량이 0이하일 때 map에서 제거해야 함
+ */
 class Solution {
-    public int solution(int[] topping) {
-        Map<Integer, Integer> mapA = new HashMap<>();
-        Map<Integer, Integer> mapB = new HashMap<>();
-        
-        for (int x: topping){
-            mapB.put(x,mapB.getOrDefault(x,0)+1);
-        }
-        
-        int cnt = 0;
-        for (int x : topping){
-            mapA.put(x,mapA.getOrDefault(x,0)+1);
-            mapB.put(x,mapB.get(x)-1);
-            if (mapB.get(x)==0) mapB.remove(x);
-            if (mapA.keySet().size()==mapB.keySet().size()){
-                cnt++;
-            }
-        }
-        
-        return cnt;
-    }
+	public int solution(int[] topping) {
+		Map<Integer, Integer> mapA = new HashMap<>();
+		Map<Integer, Integer> mapB = new HashMap<>();
+
+		for (int x: topping){
+			mapB.put(x,mapB.getOrDefault(x,0)+1);
+		}
+
+		int cnt = 0;
+		for (int x : topping){
+			mapA.put(x,mapA.getOrDefault(x,0)+1);
+			mapB.put(x,mapB.get(x)-1);
+			if (mapB.get(x)==0) mapB.remove(x); //수량 0개이면 제거
+
+			if (mapA.size()== mapB.size()) cnt++;
+		}
+
+		return cnt;
+	}
 }
