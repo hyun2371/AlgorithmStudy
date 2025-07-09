b2a924cfa0e7e24eb2486a394737a08d0697449b
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-07-09 14:40:10 +0900

    [level 2] Title: 오픈채팅방, Time: 154.12 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -2,8 +2,7 @@ import java.util.*;
 class Solution {
     public String[] solution(String[] record) {
         Map<String, String> map = new HashMap<>();
-        List<String> historyId = new ArrayList<>();
-        List<String> historyPrefix = new ArrayList<>();
+        List<Event> history = new ArrayList<>();
         
         for (int i=0;i<record.length;i++){
             String[] arr = record[i].split(" ");
@@ -13,12 +12,10 @@ class Solution {
             if (cmd.equals("Enter")) {
                 String name = arr[2];
                 map.put(id,name);
-                historyId.add(id);
-                historyPrefix.add("님이 들어왔습니다.");
+                history.add(new Event(id, "님이 들어왔습니다."));
             }
             else if (cmd.equals("Leave")) {
-                historyId.add(id);
-                historyPrefix.add("님이 나갔습니다.");
+                history.add(new Event(id, "님이 나갔습니다."));
             }
             else {
                 String name = arr[2];
@@ -26,12 +23,20 @@ class Solution {
             }
         }
         
-        String[] result = new String[historyId.size()];
+        String[] result = new String[history.size()];
         for (int i=0;i<result.length;i++){
-            result[i] = map.get(historyId.get(i))+historyPrefix.get(i);
+            Event event = history.get(i);
+            result[i] = map.get(event.id)+event.msg;
         }
         return result;
+    } 
+}
+
+class Event{
+    String id;
+    String msg;
+    public Event(String id, String msg){
+        this.id = id;
+        this.msg = msg;
     }
-    
-    
 }
