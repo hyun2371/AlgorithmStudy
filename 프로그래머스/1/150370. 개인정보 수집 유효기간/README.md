efe1130014d84583005268c8f3237f88ec1bd28e
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-08-18 22:04:59 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 1.80 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/README.md
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/README.md
@@ -1,10 +1,10 @@
 # [level 1] 개인정보 수집 유효기간 - 150370 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150370#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150370) 
 
 ### 성능 요약
 
-메모리: 83.6 MB, 시간: 1.58 ms
+메모리: 70.6 MB, 시간: 1.80 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 13일 11:03:57
+2025년 08월 18일 22:04:56
 
 ### 문제 설명
 
--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,50 +1,43 @@
 import java.util.*;
 class Solution {
-
     public int[] solution(String today, String[] terms, String[] privacies) {
         List<Integer> lst = new ArrayList<>();
-        Map<Character, Integer> map = new HashMap<>();
-        
-        String[] t = today.split("\\.");
-        int todayDays = toDays(
-            Integer.parseInt(t[0]),
-            Integer.parseInt(t[1]),
-            Integer.parseInt(t[2])
-        );
-      
-        // 약관별 유효 개월 수
-        for (int i=0;i<terms.length;i++){
-            String[] term = terms[i].split(" ");
-            map.put(term[0].charAt(0),Integer.valueOf(term[1]));
-        }
-        
+        Map<String, Integer> termMap = new HashMap<>();
+        initMap(termMap, terms);
+        int limit = getTotal(today,0);
+    
         for (int i=0;i<privacies.length;i++){
-            String[] privacy = privacies[i].split(" ");
-            String[] date = privacy[0].split("\\.");
-            int plusMonth = map.get(privacy[1].charAt(0));
-            
-            int expireDays = toDays(
-                Integer.parseInt(date[0]),
-                Integer.parseInt(date[1]),
-                Integer.parseInt(date[2])     
-            );
-            
-            expireDays+=plusMonth*28-1;
-            
-            if (expireDays<todayDays){//만료
+            String cal = privacies[i].split(" ")[0];
+            int addM = termMap.get(privacies[i].split(" ")[1]);
+            if (getTotal(cal,addM)<=limit){
                 lst.add(i+1);
             }
-        }
+        }    
+        
+        return getAnswer(lst);
+    }
+    
+    private int[] getAnswer(List<Integer> lst){
         int[] answer = new int[lst.size()];
-        for (int i=0;i<answer.length;i++){
+        for (int i=0;i<lst.size();i++){
             answer[i] = lst.get(i);
         }
-    
         return answer;
     }
     
+    private static void initMap(Map<String,Integer> termMap, String[] terms){
+         for (int i=0;i<terms.length;i++){
+            termMap.put(terms[i].split(" ")[0],Integer.parseInt(terms[i].split(" ")[1]));
+        }
+        System.out.println(termMap);
+    }
     
-   private int toDays(int y, int m, int d) {
-        return y * 12 * 28 + (m-1) * 28 + d;
+    private static int getTotal(String cal, int addM){
+        int total = addM*28;
+        String[] date = cal.split("\\.");
+        return total
+            +Integer.valueOf(date[0])*12*28
+            +Integer.valueOf(date[1])*28
+            +Integer.valueOf(date[2]);
     }
 }
