d4e8b129dec7186dad326472c29201ae2f0adb64
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-08-22 21:26:50 +0900

    [Silver III] Title: 계단 오르기, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -4,25 +4,27 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int[][] d = new int[N + 1][3];
-		int[] score = new int[N + 1];
-		for (int i = 1; i <= N; i++) {
-			score[i] = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+		int[][] d = new int[n + 1][3];
+		int[] s = new int[n + 1];
+
+		for (int i = 1; i <= n; i++) {
+			s[i] = Integer.parseInt(br.readLine());
 		}
-        
-        if (N==1){
-			System.out.println(score[1]);
+		if (n==1) {
+			System.out.println(s[1]);
 			return;
 		}
+		d[1][1] = s[1];
+		d[1][2] = 0;
 
-		d[1][1] = score[1]; d[1][2] = 0;
-		d[2][1] = score[2]; d[2][2] = score[1]+score[2];
+		d[2][1] = s[2];
+		d[2][2] = s[1] + s[2];
 
-		for (int i=3;i<=N;i++){
-			d[i][1] = Math.max(d[i-2][1],d[i-2][2])+score[i];
-			d[i][2] = d[i-1][1]+score[i];
+		for (int i = 3; i <= n; i++) {
+			d[i][1] = Math.max(d[i - 2][1], d[i - 2][2]) + s[i];
+			d[i][2] = d[i - 1][1] + s[i];
 		}
-		System.out.println(Math.max(d[N][1], d[N][2]));
+		System.out.println(Math.max(d[n][1], d[n][2]));
 	}
 }
