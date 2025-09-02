059f9aae7c5d4dbf2b7575eca4b8c280ad9a15ef
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-09-02 20:55:11 +0900

    [level 2] Title: 무인도 여행, Time: 5.57 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/README.md
+++ 프로그래머스/2/154540. 무인도 여행/README.md
@@ -1,10 +1,10 @@
 # [level 2] 무인도 여행 - 154540 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154540) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154540?language=java) 
 
 ### 성능 요약
 
-메모리: 88.4 MB, 시간: 5.44 ms
+메모리: 84.9 MB, 시간: 5.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 26일 18:56:56
+2025년 09월 02일 20:55:10
 
 ### 문제 설명
 
--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -1,6 +1,59 @@
+import java.util.*;
 class Solution {
+    static int N,M;
+    static int[][] board;
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+    static int sum;
+    static List<Integer> days = new ArrayList<>();
     public int[] solution(String[] maps) {
-        int[] answer = {};
+        N = maps.length;
+        M = maps[0].length();
+        board = new int[N][M];
+        initBoard(maps);
+        
+        for (int i=0;i<N;i++){
+            for (int j=0;j<M;j++){
+                if (board[i][j]>0) {
+                    DFS(i,j);
+                    days.add(sum);
+                    sum=0;
+                } 
+            }
+        }
+        
+        if (days.isEmpty()) return new int[]{-1};
+        Collections.sort(days);
+        int[] answer = new int[days.size()];
+        for (int i=0;i<days.size();i++){
+            answer[i] = days.get(i);
+        }
         return answer;
+    }
+    
+    static void initBoard(String[] maps){
+        for (int i=0;i<N;i++){
+            String s = maps[i];
+            for (int j=0;j<M;j++){
+                char c = s.charAt(j);
+                if (c=='X') board[i][j] = -1;
+                else board[i][j] = c-'0';
+            }
+        }
+    }
+    
+    static void DFS(int x, int y){
+        sum+=board[x][y];
+        board[x][y] = 0; //방문 처리
+        for (int d=0;d<dx.length;d++){
+            int nx = x+dx[d];
+            int ny = y+dy[d];
+            if (!inRange(nx,ny)||board[nx][ny]<=0) continue;
+            DFS(nx,ny);
+        }
+    }
+    
+    static boolean inRange(int x, int y){
+        return x>=0&&x<N&&y>=0&&y<M;
     }
 }
