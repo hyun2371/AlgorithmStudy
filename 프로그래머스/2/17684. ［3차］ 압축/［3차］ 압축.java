d03de5c33bc5d90e0e827bb106cd4e62e2f4ae76
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-09-04 22:18:46 +0900

    [level 2] Title: [3차] 압축, Time: 10.20 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -1,37 +1,38 @@
 import java.util.*;
 class Solution {
+    static Map<String, Integer> map = new HashMap<>();
     public int[] solution(String msg) {
-        Map<String, Integer> dict = new HashMap<>();
-        List<Integer> output = new ArrayList<>();
-        initDict(dict);     
-        int curIdx = 0, dictIdx=27;
+        initMap();
+        List<Integer> lst = new ArrayList<>();
         
+        int curIdx = 0, dictIdx = 27;
         while (curIdx<msg.length()){
             int nextIdx = curIdx+1;
             String w = ""+msg.charAt(curIdx);
-            while (nextIdx<=msg.length()&&dict.containsKey(msg.substring(curIdx,nextIdx))){          
-                w = msg.substring(curIdx,nextIdx);
+            while (nextIdx<=msg.length()&&map.containsKey(msg.substring(curIdx,nextIdx))){
+                w = msg.substring(curIdx, nextIdx);
                 nextIdx++;
             }
-            output.add(dict.get(w)); // 기존 단어 찾기
-            
-            if (nextIdx<=msg.length()) 
-                dict.put(msg.substring(curIdx,nextIdx),dictIdx++); //새로운 단어 추가
-            curIdx += w.length();
+
+            lst.add(map.get(w));
+            if (nextIdx<=msg.length()){
+                map.put(msg.substring(curIdx, nextIdx),dictIdx++);
+            }
+            curIdx +=w.length();
         }
+
         
-        int[] answer = new int[output.size()];
-        for (int i=0;i<output.size();i++){
-            answer[i] = output.get(i);
+        
+        int[] answer = new int[lst.size()];
+        for (int i=0;i<lst.size();i++){
+            answer[i] = lst.get(i);
         }
-        
         return answer;
     }
     
-    private static void initDict(Map<String, Integer> dict){
-        int dictIdx = 1;
-        for (char c = 'A';c<='Z';c++){
-            dict.put(c+"",dictIdx++);
+    static void initMap(){
+        for (int i=0;i<26;i++){
+            map.put(String.valueOf((char)('A'+i)),i+1);
         }
     }
 }
