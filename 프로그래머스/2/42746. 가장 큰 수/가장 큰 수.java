5cb8c0cd1e696e5ad9959b8d7206adf7e1dd7f95
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-09-25 21:43:28 +0900

    [level 2] Title: 가장 큰 수, Time: 242.79 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,29 +1,22 @@
 import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
-        List<Num> lst = new ArrayList<>();
-        for (int x : numbers){
-            lst.add(new Num(x));
+        String[] nums = new String[numbers.length];
+        for (int i=0;i<nums.length;i++){
+            nums[i] = numbers[i]+"";
         }
-        Collections.sort(lst);
-        if (lst.get(0).number==0) return "0";
+        
+        Arrays.sort(nums,new Comparator<String>(){
+            public int compare(String o1, String o2){
+                return (o2+o1).compareTo(o1+o2);
+            }
+        });
+        
+        if (nums[0].equals("0")) return "0";
         StringBuilder sb = new StringBuilder();
-        for (Num num : lst){
-            sb.append(num.number);
+        for (String s : nums){
+            sb.append(s);
         }
         return sb.toString();
-    }
-    
-}
-
-class Num implements Comparable<Num>{
-    int number;
-    public Num(int number){
-        this.number = number;
-    }
-    public int compareTo(Num o){
-        String n1 = this.number+""+o.number;
-        String n2 = o.number+""+this.number; 
-        return n2.compareTo(n1);
     }
 }
