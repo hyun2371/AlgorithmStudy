36f40cae834826fad72fe9f07d99b520254e2a7d
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-10-05 23:38:43 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -0,0 +1,24 @@
+-- 코드를 작성해주세요
+SELECT
+    CASE
+        WHEN SUM(CASE WHEN SC.NAME = 'Python' THEN 1 ELSE 0 END) > 0
+             AND SUM(CASE WHEN SC.CATEGORY = 'Front End' THEN 1 ELSE 0 END) > 0
+             THEN 'A'
+        WHEN SUM(CASE WHEN SC.NAME = 'C#' THEN 1 ELSE 0 END) > 0
+             THEN 'B'
+        WHEN SUM(CASE WHEN SC.CATEGORY = 'Front End' THEN 1 ELSE 0 END) > 0
+             THEN 'C'
+        ELSE NULL
+    END AS GRADE,
+    D.ID,
+    D.EMAIL
+FROM
+    DEVELOPERS D
+JOIN
+    SKILLCODES SC ON D.SKILL_CODE & SC.CODE <> 0
+GROUP BY
+    D.ID, D.EMAIL
+HAVING
+    GRADE IS NOT NULL
+ORDER BY
+    GRADE, D.ID;
