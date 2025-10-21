aa8b3a65b3227062741b7d5311a9254f089522e1
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-10-21 11:59:00 +0900

    [level 2] Title: [1차] 캐시, Time: 34.38 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,28 +1,31 @@
 import java.util.*;
 class Solution {
-    static final int HIT_TIME = 1, MISS_TIME = 5;
+    static int totalT;
+    static Queue<String> q = new LinkedList<>();
     public int solution(int cacheSize, String[] cities) {
-        if (cacheSize==0) return cities.length*MISS_TIME;
-        
-        // 캐시 초기화
-        List<String> lst = new LinkedList<>();
-        int time = 0;
-        
-        
-        for (String city:cities){
-            String key = city.toLowerCase();
-            if (lst.contains(key)){ //캐시 hit
-                lst.remove(key);
-                lst.add(key);
-                time+=HIT_TIME;
-            } else {//캐시 miss
-                if (lst.size()>=cacheSize){
-                    lst.remove(0); //기존꺼 제거
-                }
-                lst.add(key);
-                time+=MISS_TIME;
-            }
+        if (cacheSize==0)
+            return cities.length*5;
+        for (String key : cities){
+            search(cacheSize, key.toLowerCase());
         }
-        return time;
+        return totalT;
+    }
+    
+    private static void search(int cacheSize, String key){
+        if (q.contains(key)) {
+            totalT++;
+            q.remove(key);
+            q.offer(key);
+        }
+        else {
+            if (q.size()<cacheSize){
+                q.offer(key);
+            }
+            else {
+                q.poll();
+                q.offer(key);
+            }
+            totalT+=5;
+        }
     }
 }
