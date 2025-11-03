b021e37b2aec01101546d6702940a54915a0cd46
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-11-03 21:17:31 +0900

    [Silver II] Title: 연속합, Time: 224 ms, Memory: 24148 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,21 +1,24 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
-		int[] d = new int[N];
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
+		for (int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
+
+		int[] d = new int[n];
 		d[0] = arr[0];
 		int answer = d[0];
-		for (int i = 1; i < N; i++) {
-			d[i] = Math.max(d[i - 1], 0)+arr[i];
-			answer = Math.max(d[i], answer);
+		for (int i = 1; i < n; i++) {
+			d[i] = Math.max(d[i - 1], 0) + arr[i];
+			answer = Math.max(answer, d[i]);
 		}
-		System.out.println(answer);
+		System.out.print(answer);
 	}
 }
