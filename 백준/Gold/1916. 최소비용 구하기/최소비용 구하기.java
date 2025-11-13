6ea345dea18722670747a2aa9305c7796fb85479
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-11-13 10:12:59 +0900

    [Gold V] Title: 최소비용 구하기, Time: 460 ms, Memory: 50832 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,61 +1,52 @@
 import java.util.*;
 import java.io.*;
-
 public class Main {
-	static int N, M;
 	static int[] dis;
 	static List<List<Edge>> graph = new ArrayList<>();
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		M = Integer.parseInt(br.readLine());
-		dis = new int[N + 1];
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+
+		dis = new int[n+1];
 		Arrays.fill(dis, Integer.MAX_VALUE);
 
-		for (int i = 0; i <= N; i++) {
+		for (int i = 0; i <= n; i++) {
 			graph.add(new ArrayList<>());
 		}
-
-		StringTokenizer st;
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < m; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
-			graph.get(x).add(new Edge(y, c));
+			graph.get(x).add(new Edge(y,c));
 		}
 
-		st = new StringTokenizer(br.readLine());
-		int start = Integer.parseInt(st.nextToken());
-		solution(start);
-		int target = Integer.parseInt(st.nextToken());
-		System.out.println(dis[target]);
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int s = Integer.parseInt(st.nextToken());
+		int t = Integer.parseInt(st.nextToken());
+		dijkstra(s);
+		System.out.print(dis[t]);
 	}
 
-	private static void solution(int v) {
+	private static void dijkstra(int s){
 		Queue<Edge> pq = new PriorityQueue<>();
-		dis[v] = 0;
-		pq.offer(new Edge(v, 0));
-
-
+		pq.offer(new Edge(s,0));
+		dis[s] = 0;
 		while (!pq.isEmpty()) {
-			Edge cur = pq.poll();
-			int curVertex = cur.vertex;
-			int curCost = cur.cost;
-			if (curCost > dis[curVertex]) continue;
-
-			for (Edge e : graph.get(curVertex)) {
-				if (dis[curVertex]+e.cost<dis[e.vertex]){
-					dis[e.vertex] = dis[curVertex]+e.cost;
-					pq.offer(new Edge(e.vertex,dis[e.vertex]));
+			Edge cur = pq.poll(); //현재 정점
+			if (cur.cost>dis[cur.vertex])continue;
+			for (Edge e : graph.get(cur.vertex)) {//꺼내는 정점
+				if (dis[e.vertex]>cur.cost+e.cost){
+					dis[e.vertex] = cur.cost+e.cost;
+					pq.offer(new Edge(e.vertex, dis[e.vertex]));
 				}
 			}
 		}
 	}
 }
 
-class Edge implements Comparable<Edge> {
+class Edge implements Comparable<Edge>{
 	int vertex;
 	int cost;
 
@@ -65,7 +56,7 @@ class Edge implements Comparable<Edge> {
 	}
 
 	@Override
-	public int compareTo(Edge e) {
-		return this.cost - e.cost;
+	public int compareTo(Edge o){
+		return this.cost-o.cost;
 	}
 }
