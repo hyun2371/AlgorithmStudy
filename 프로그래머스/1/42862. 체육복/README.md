fba71430157d27c1b6aecf31c617bc5a60bdcb26
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-11-19 20:03:13 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#) 
 
 ### 성능 요약
 
-메모리: 85.1 MB, 시간: 0.55 ms
+메모리: 84.7 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 19일 19:45:49
+2025년 11월 19일 20:03:11
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,28 +1,23 @@
 import java.util.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        Arrays.sort(lost);
-        boolean[] isLeft = new boolean[n+1]; //여벌 있는지
-        int unwearCnt = 0; 
-        Set<Integer> lostSet = new HashSet<>();
-        for (int id : lost)lostSet.add(id); //잃어버림
+        int answer = n;
+        int[] cnt = new int[n+1]; //체육복 보유 개수  1여분 0자기꺼만 -1분실
         
-        //자기 여벌 처리
-        for (int id : reserve){
-            if (lostSet.contains(id)){ //여벌 사용
-                lostSet.remove(id);
-            } else {  //도난 당하지 않아 사용할 필요없음
-                isLeft[id] = true;
-            }
+        for (int ind : lost){//분실
+            cnt[ind]--;
+        }
+        for (int ind : reserve){//여분
+            cnt[ind]++;
         }
         
-        for (int id : lost){//잃어버린 학생에 대해
-            if (!lostSet.contains(id)) continue; //자기 여벌 사용한 학생 제외
-            if (id>1&&isLeft[id-1]) isLeft[id-1] = false; //왼쪽에서 빌릴 수 있는지
-            else if (id<n&&isLeft[id+1]) isLeft[id+1] = false; //오른쪽에서 빌릴 수 있는지
-            else unwearCnt++; //못 빌림
+        for (int i=1;i<=n;i++){
+            if (cnt[i]>=0) continue;
+            if (i>1&&cnt[i-1]>0) cnt[i-1]--;
+            else if (i<n&&cnt[i+1]>0) cnt[i+1]--;
+            else answer--;
         }
         
-        return n-unwearCnt; //전체-못 빌린 수
+        return answer;
     }
 }
