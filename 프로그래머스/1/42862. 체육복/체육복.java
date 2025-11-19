3ee96893e376cf08891f426d5c83dd142a01b39d
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-11-19 19:45:51 +0900

    [level 1] Title: 체육복, Time: 0.55 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,26 +1,28 @@
 import java.util.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int count = 0;
-        int[] arr = new int[n+1];
-        for (int ind : lost){
-            arr[ind]--;
-        }
-        for (int ind: reserve){
-            arr[ind]++;
-        }
-        for (int i=1;i<=n;i++){
-            if (arr[i]>-1) count++;
-            else {
-                if (arr[i-1]==1){ // 왼쪽에서 빌림
-                    arr[i-1]--; count++; continue;
-                }
-                if (i+1<=n&&arr[i+1]==1){ // 오른쪽에서 빌림
-                    arr[i+1]--; count++;
-                }
+        Arrays.sort(lost);
+        boolean[] isLeft = new boolean[n+1]; //여벌 있는지
+        int unwearCnt = 0; 
+        Set<Integer> lostSet = new HashSet<>();
+        for (int id : lost)lostSet.add(id); //잃어버림
+        
+        //자기 여벌 처리
+        for (int id : reserve){
+            if (lostSet.contains(id)){ //여벌 사용
+                lostSet.remove(id);
+            } else {  //도난 당하지 않아 사용할 필요없음
+                isLeft[id] = true;
             }
         }
-        System.out.println(Arrays.toString(arr));
-        return count;
+        
+        for (int id : lost){//잃어버린 학생에 대해
+            if (!lostSet.contains(id)) continue; //자기 여벌 사용한 학생 제외
+            if (id>1&&isLeft[id-1]) isLeft[id-1] = false; //왼쪽에서 빌릴 수 있는지
+            else if (id<n&&isLeft[id+1]) isLeft[id+1] = false; //오른쪽에서 빌릴 수 있는지
+            else unwearCnt++; //못 빌림
+        }
+        
+        return n-unwearCnt; //전체-못 빌린 수
     }
 }
