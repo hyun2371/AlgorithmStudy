4a74f0b26c430378a892fe5fc3fb45e54f4148e9
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-11-19 17:58:40 +0900

    [Silver II] Title: 비슷한 단어, Time: 172 ms, Memory: 17620 KB -BaekjoonHub

--- 백준/Silver/2607. 비슷한 단어/비슷한 단어.java
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.java
@@ -1,34 +1,37 @@
 import java.util.*;
+import java.io.*;
+
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int[] origin = new int[26];
-		for (char c : sc.next().toCharArray()) {
-			origin[c-'A']++;
+		String origin = sc.next();
+		int[] alpha = new int[26];
+		for (char c : origin.toCharArray()) {
+			alpha[c-'A']++;
 		}
 		int answer = 0;
 		for (int i = 1; i < n; i++) {
-			String s = sc.next();
-			if (isSimilar(s, origin)) answer++;
+			String cmp = sc.next();
+			if (isSimilar(origin, cmp, alpha)) answer++;
 		}
-		System.out.println(answer);
+		System.out.print(answer);
 	}
-	private static boolean isSimilar(String s, int[] origin){
-		int[] copy = Arrays.copyOf(origin, origin.length);
-
-		int extra = 0;
-		int missing = 0;
-
-		for (char c : s.toCharArray()) {
-			int idx = c-'A';
-			if (copy[idx]>0) copy[idx]--;
-			else extra++;
+	private static boolean isSimilar(String origin, String cmp, int[] alpha){
+		int[] copy = Arrays.copyOf(alpha, alpha.length);
+		int matchCnt=0;
+		for (char c : cmp.toCharArray()){
+			if (copy[c-'A']>0) {
+				copy[c-'A']--;
+				matchCnt++;
+			}
 		}
-
-		for (int cnt:copy){
-			missing+=cnt;
-		}
-		return extra<=1&&missing<=1;
+		//i) 문자 하나 더하면 원본과 일치
+		if (cmp.length()==origin.length()-1&&matchCnt==origin.length()-1) return true;
+		//ii) 문자 하나 빼면 원본과 일치
+		if (cmp.length()==origin.length()+1&&matchCnt==origin.length()) return true;
+		//iii) 완전 일치하거나 문자열 하나 대체
+		if (cmp.length()==origin.length()&&(matchCnt==origin.length()||matchCnt==origin.length()-1)) return true;
+		return false;
 	}
 }
