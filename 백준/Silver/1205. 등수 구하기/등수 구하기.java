238dc3b52f29e9610805570c5b8e10affb22faa5
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-11-26 22:37:02 +0900

    [Silver IV] Title: 등수 구하기, Time: 104 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Silver/1205. 등수 구하기/등수 구하기.java
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.java
@@ -9,23 +9,25 @@ public class Main {
 		int target = Integer.parseInt(st.nextToken());
 		int P = Integer.parseInt(st.nextToken());
 
+		if (N==0){
+			System.out.print(1); return;
+		}
 		int[] arr = new int[N];
-		if (N>0)
-			st = new StringTokenizer(br.readLine());
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 
-		Arrays.sort(arr);
-		int rank = N+1, sameCnt =0;
-		for (int i=N-1;i>=0;i--){
-			if (target>=arr[i]&&rank==N+1) rank = N-i;
-			if (target==arr[i]) sameCnt++;
+		int rank = 1;
+		for (int i = 0; i < N; i++) {
+			if (arr[i]>target) rank++;
+			else break;
 		}
-		int answer;
-		if (rank>P) answer = -1;
-		else if (sameCnt>0&&rank+sameCnt>P) answer = -1;
-		else answer = rank;
-		System.out.print(answer);
+        
+        if ((rank>P)||N>=P&&target<=arr[P-1]){
+			System.out.print(-1);
+			return;
+		}
+		System.out.print(rank);
 	}
 }
