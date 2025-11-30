40c085123b1c2452f7ba35157d9a2c5bed0d6604
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-11-30 10:21:02 +0900

    [Silver V] Title: 방 번호, Time: 156 ms, Memory: 17664 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -1,25 +1,18 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		String s = String.valueOf(n);
-		int[] arr = new int[10]; 
-
-		for (int i = 0; i < s.length(); i++) {
-			char c = s.charAt(i);
-			if (c=='6'||c=='9'){
-				if (arr[6]>arr[9]) arr[9]++;
-				else arr[6]++;
-			}
-			else {
-				arr[c-'0']++;
-			}
-		}
-
-		Arrays.sort(arr);
-		System.out.println(arr[9]);
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       String s = sc.next();
+       int[] cnt = new int[10]; //0~9
+       for (char c : s.toCharArray()){
+           if (c=='6'||c=='9'){
+               if (cnt[6]>cnt[9]) cnt[9]++;
+               else cnt[6]++;
+           }
+          else cnt[c-'0']++;
+       }
+       Arrays.sort(cnt);
+       System.out.print(cnt[9]);
+    }
 }
