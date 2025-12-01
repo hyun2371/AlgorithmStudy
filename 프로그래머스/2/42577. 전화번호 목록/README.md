db81f41dc989c2550c8b142758ebc2d51e5b8f7c
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-12-01 19:10:48 +0900

    [level 2] Title: 전화번호 목록, Time: 375.98 ms, Memory: 98.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?gad_source=1&gad_campaignid=22199869887&gbraid=0AAAAAC_c4nC4ik940U5PpEi-tvbWJ4MGG&gclid=CjwKCAiAlrXJBhBAEiwA-5pgwub46bgog3bUMkX5JW78zX2RL7sWxO8zbC-8tIjZORPw-wjgdJPgfRoCgTEQAvD_BwE) 
 
 ### 성능 요약
 
-메모리: 249 MB, 시간: 311.40 ms
+메모리: 98.6 MB, 시간: 375.98 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 15일 23:50:53
+2025년 12월 01일 19:10:45
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,20 +1,14 @@
 import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
-         Set<String> set = new HashSet<>();
+        boolean answer = true;
+        Arrays.sort(phone_book);
         
-        for (String phone : phone_book) {
-            set.add(phone);
-        }
-        
-        for (String phone : phone_book) {
-            for (int i = 1; i < phone.length(); i++) {
-                if (set.contains(phone.substring(0, i))) {
-                    return false;
-                }
+        for (int i=0;i<phone_book.length-1;i++){
+            if (phone_book[i+1].startsWith(phone_book[i])) {
+                answer = false; break;
             }
         }
-        
-        return true;
+        return answer;
     }
 }
