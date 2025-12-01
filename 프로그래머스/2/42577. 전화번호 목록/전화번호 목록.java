9caa99050a0996207e285c4038d970def3be94fd
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2025-12-01 19:37:30 +0900

    [level 2] Title: 전화번호 목록, Time: 363.58 ms, Memory: 206 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,13 +2,19 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        Arrays.sort(phone_book);
+        Set<String> set = new HashSet<>();
+        for (String s : phone_book){
+            set.add(s);
+        }
         
-        for (int i=0;i<phone_book.length-1;i++){
-            if (phone_book[i+1].startsWith(phone_book[i])) {
-                answer = false; break;
+        for (String s : phone_book){
+            for (int i=0;i<s.length();i++){
+                if (set.contains(s.substring(0,i))){
+                    answer = false; break;
+                }
             }
         }
+        
         return answer;
     }
 }
